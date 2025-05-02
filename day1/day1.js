502298543b7a73a23cd1688b797d44bb5eca02a5
Nguyen Minh Kha <minhkha20190@gmail.com>
2025-05-02 20:23:17 +0700

    hoc javascript

---
+++ day1/day1.js
@@ -0,0 +1,20 @@
+// const x = 5; const = hằng số không đổi
+
+
+let x = 5, y = 10 , z = 15;
+let e = x + y + z;
+console.log("Test string" , 'x=', x,'y=',y,'z=',z);
+console.log("Test string" , 'x=' + x +' y=' + y + ' z=' + z);
+console.log("e=", e);
+
+let a= ' Tôi là chuỗi'
+
+console.log(a)
+// cộng từ trái sang phải
+console.log( 4 + 5 +'a'); // = 9a
+console.log( 'a' + 4 + 5); // = a45
+
+let text='asdavsdfsfas dasdasd'
+//trim() xóa khoảng trắng ở đầu và cuối chuỗi
+//length trả về độ dài của chuỗi
+console.log(text.trim().length);
