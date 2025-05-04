3e66418cee29bfe2211979bde4cb485de0d12472
Nguyen Minh Kha <minhkha20190@gmail.com>
2025-05-04 20:27:52 +0700

    hoc javascript

---
+++ day2/day2.js
@@ -0,0 +1,8 @@
+console.log('Helo world!' .toUpperCase());
+let a= 5;
+let b= 10;
+let c= 15;
+let d= '20';
+let e= +d; // convert string to number
+console.log('e value:',e, 'e type:', typeof e);
+console.log(a===d, 'a:', typeof a, 'd:', typeof d);
